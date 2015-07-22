64b0ba48b73a7abd0b54bbe6ec3955f0562e4885
Can <canfirtina@gmail.com>
2015-07-22 13:17:56 +0300

    Initial Commit

---
+++ FirstOpenCLProgram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FirstOpenCLProgram
+//
+//  Created by Can Firtina on 22/07/15.
+//  Copyright (c) 2015 Can Firtina. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
